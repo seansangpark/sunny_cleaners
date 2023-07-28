cfc7904d7a4b7a404f524b8ecafeaff376ab54d8
Sean Park <seansangpark@gmail.com>
2023-07-28 14:22:48 -0400

    minor edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import styles from '../style';
+import { logo } from '../assets';
+import { footerLinks, socialMedia } from '../constants';
 
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
