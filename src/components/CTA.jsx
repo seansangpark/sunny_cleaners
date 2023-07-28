520f864009b4bc21d9a3fc8d03bc2d5328f7c2d4
Sean Park <seansangpark@gmail.com>
2023-07-28 13:35:23 -0400

    CTA edited

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -6,10 +6,10 @@ const CTA = () => (
     className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}
   >
     <div className='flex-1 flex flex-col'>
-      <h2 className={styles.heading2}>Let's try our service now!</h2>
+      <h2 className={styles.heading2}>Come visit us now!</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        Everything you need to accept card payments and grow your business
-        anywhere on the planet.
+        Our team are laundry and dry cleaning experts and ensure the highest
+        standards of hygiene for your clothing, bedding, and household.
       </p>
     </div>
     <div className={`${styles.flexCenter} sm:ml-10 ml-9 sm:mt-0 mt-10`}>
